e2358e892ddcfa4cdb00a67a4d962434f63606d1
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-10-02 22:12:35 -0400

    Cleanup

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftCellsAndStuff",
-    products: [
-        .library(
-            name: "SwiftCellsAndStuff",
-            targets: ["SwiftCellsAndStuff"]
-        ),
-    ],
     dependencies: [],
     targets: [
         .target(name: "SwiftCellsAndStuff",
