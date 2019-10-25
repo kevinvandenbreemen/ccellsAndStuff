bc3b465c55bac8e98028dc8b1b757bd9777a6de8
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-10-24 20:22:47 -0400

    Include non-debug flag when building C code into Swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "cCellsAndStuff",
-            path: "./Sources/cCellsAndStuff"
+            path: "./Sources/cCellsAndStuff",
+            cSettings: [
+                .define("NDEBUG")
+            ]
         ),
         .target(
             name: "cellsAndStuff",
