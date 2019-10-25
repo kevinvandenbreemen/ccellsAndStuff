cf6c64a108b4bd758c51d9d46afc4590392b92a4
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-10-24 20:40:58 -0400

    Expose functionality

--- Sources/cellsAndStuff/TissueManager.swift
+++ Sources/cellsAndStuff/TissueManager.swift
@@ -1,12 +1,12 @@
 import cCellsAndStuff
 
-class TissueManager {
+public class TissueManager {
 
-    func initializeDefaultTissue() {
+    public func initializeDefaultTissue() {
         cCellsAndStuff.swift_tissue_initializeDefaultTissue()
     }
 
-    func connectCell(from: Int32, to: Int32, strength: Double) {
+    public func connectCell(from: Int32, to: Int32, strength: Double) {
         let result = cCellsAndStuff.swift_cells_connectDirected(from, to, strength)
         if result != 0 {
             print("Failed to Connect cells - see log for details")
--- Sources/cellsAndStuff/cellsAndStuff.swift
+++ Sources/cellsAndStuff/cellsAndStuff.swift
@@ -1,15 +1,15 @@
 import cCellsAndStuff
 
-struct CellsAndStuffSystemAbout {
+public struct CellsAndStuffSystemAbout {
     
-    func versionNum() -> String {
+    public func versionNum() -> String {
         let majorVer = cCellsAndStuff.majorVersion()
         let minorVer = cCellsAndStuff.minorVersion()
         let revNum = cCellsAndStuff.revisionNum()
         return "\(majorVer).\(minorVer).\(revNum)"
     }
 
-    func getAbout() -> String {
+    public func getAbout() -> String {
         return "C Cells and Stuff Integration Lib:  v.\(versionNum())"
 
     }
