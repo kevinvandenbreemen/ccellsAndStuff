0be75c8219f9df100174a78e465ddcc27e58bd01
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-11-06 21:50:15 -0500

    Display grid of cells

--- include/tissue_display.h
+++ include/tissue_display.h
@@ -2,6 +2,10 @@
 #define IN_DISPLAY
 
 #include <SDL2/SDL.h>
+#include <math.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 /**
  * Display the current tissue
@@ -23,7 +27,7 @@ static BitArray * getDisplaySettings() {
         ret = bitarray_create(2);
         initialized = 1;
     }
-    
+
     return ret;
 }
 
@@ -42,8 +46,7 @@ static void doSDLSetup() {
 }
 
 static SDL_Window * getWindow() {
-    const int SCREEN_WIDTH = 640;
-    const int SCREEN_HEIGHT = 480;
+
 
     static SDL_Window *window;
 
@@ -77,6 +80,64 @@ static SDL_Renderer * getRenderer() {
     return renderer;
 }
 
+static void drawCells(SDL_Renderer *renderer) {
+
+    int numCells = tissue_getNumCells();
+    int root = (int) ceil(sqrt((double)numCells));
+    int area = SCREEN_WIDTH * SCREEN_HEIGHT;
+    int cellHeight = (int)floor(SCREEN_HEIGHT / root);
+    int cellWidth = (int)floor(SCREEN_WIDTH / root);
+
+    #ifndef NDEBUG
+    printf("Render Cells: root=%d, area=%d, cellSize[w,h]=(%d, %d)\n", root, area, cellWidth, cellHeight);
+    #endif
+
+    TissueState *state = tissue_getState();
+    BitArray *activeCells = bitarray_create(numCells);
+    
+
+    int i, j, index;
+
+    for(i=0; i<state->outputCount; i++) {
+        bitarray_writeBit(activeCells, state->outputIndices[i], on);
+    }
+
+    SDL_Rect rect;
+    index = 0;
+    for(i=0; i<root; i++) {
+        for(j=0; j<root; j++) {
+
+            if(index >= numCells) {
+                break;
+            }
+
+            rect.x = (j * cellWidth);
+            rect.y = (i * cellHeight);
+            rect.h = cellHeight - 5;
+            rect.w = cellWidth - 5;
+
+            #ifdef DEBUG
+            printf("render rect = {x=%d, y=%d, h=%d, w=%d}\n", rect.x, rect.y, rect.h, rect.w);
+            #endif
+
+            if(bitarray_valueOf(activeCells, index) == on){
+                SDL_SetRenderDrawColor(renderer, 255, 233, 233, 100);
+            } else {
+                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 100);
+            }
+
+            SDL_RenderFillRect(renderer, &rect);
+
+            index ++;
+        }
+    }
+
+    bitarray_destroy(activeCells);
+    free(state->outputIndices);
+    free(state->outputStrengths);
+    free(state);
+}
+
 void tissue_close_display() {
     SDL_DestroyRenderer(getRenderer());
     SDL_DestroyWindow(getWindow());
@@ -102,6 +163,8 @@ void tissue_display_tissue() {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     
     SDL_RenderDrawPoint(renderer, 100, 100);
+
+    drawCells(renderer);
 
     //	Show what was drawn
     SDL_RenderPresent(renderer);
--- main.c
+++ main.c
@@ -16,6 +16,19 @@ int main() {
     printf("This is a project for learning about and using array functions to create relating cell things\n");
     tissue_initializeDefaultTissue();
 
+    cells_connectDirected(0, 10, 0.24);
+    cells_connectDirected(1, 10, 2.2);
+    cells_connectDirected(2, 10, -0.4);
+    cells_connectDirected(3, 10, 4.9);
+    cells_connectDirected(2, 3, 4.9);
+
+    int targets[] = {0,1,2,3};
+    double strengths[] = {1.0, 1.0, 10.3, 1.2};
+    int count = 4;
+    
+    cells_stimulate(targets, strengths, count);
+    cells_matrix_feedforward_stim(targets, strengths, count);
+
     tissue_display_tissue();
     SDL_Delay(5000);
     tissue_display_tissue();
